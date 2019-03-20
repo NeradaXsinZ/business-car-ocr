384cdd592930ee356e44323481424fd2d17d1b4f
Reginald Nettey <rn4187@cs.ship.edu>
2019-03-19 20:01:04 -0400

    dev, added more tests in EmailParserTests

--- src/test/java/businesscardocr/EmailParserTests.java
+++ src/test/java/businesscardocr/EmailParserTests.java
@@ -22,7 +22,34 @@ public class EmailParserTests {
 		 line = "my.email@example.com";
 		 lineContainsEmailAddress(line, expectedEmail);
 	}
+	@Test
+	public void testProcessComplexLine() {
+		 expectedEmail = "my.email@example.com";
+		 line =  String.format("Hi, my name is example and my email address is"
+		 		+ " %s. Please email me at your leisure.", expectedEmail);
+		 lineContainsEmailAddress(line, expectedEmail);
+	}
 	
+	@Test 
+	public void testEmailWithSymbols() {
+		 expectedEmail = "#*&(*&@example.com";
+		 line = String.format("Email:  %s",expectedEmail);
+		 lineContainsEmailAddress(line, expectedEmail);
+	}
+	@Test 
+	public void testFailure_returnsEmptyString() {
+		 expectedEmail = "";
+		 line = "Email:  my.email.example.com@.com";
+		 lineContainsEmailAddress(line, expectedEmail);
+	}
+	
+	@Test 
+	public void testProcessLineFails_whenAtIsMissing() {
+		 expectedEmail = "";
+		 line = "Email:  my.email.example.com.com";
+		 lineContainsEmailAddress(line, expectedEmail);
+	}
+		
 	public void lineContainsEmailAddress(String line, String expectedEmail) {
 		String actualEmail = emailParser.processLine(line);
 		assertEquals(expectedEmail, actualEmail);
