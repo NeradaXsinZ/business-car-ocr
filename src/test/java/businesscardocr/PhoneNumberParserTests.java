949b61f52140ef7488cd23bfc522a7b70ebe42b0
Reginald Nettey <rn4187@cs.ship.edu>
2019-03-20 16:21:13 -0400

    dev, wrote tests for PhoneNumberParser

--- src/test/java/businesscardocr/PhoneNumberParserTests.java
+++ src/test/java/businesscardocr/PhoneNumberParserTests.java
@@ -7,13 +7,76 @@ import org.junit.Test;
 
 public class PhoneNumberParserTests {
 
+	private String line;
+	private String expectedPhoneNumber;
+	private Parser phoneNumberParser;
+
 	@Before
-	public void setUp() throws Exception {
+	public void setup() {
+		 phoneNumberParser = new PhoneNumberParser();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testProcessLine_returnsPhoneNumber() {
+		expectedPhoneNumber = "7177249371";
+		line = "7177249371";
+		lineContainsPhoneNumber(line, expectedPhoneNumber);
 	}
 
+	@Test
+	public void testLineWithFormatting() {
+		expectedPhoneNumber = "7177249371";
+		line = String.format("(717) 724-9371");
+		lineContainsPhoneNumber(line, expectedPhoneNumber);
+	}
+	
+	@Test
+	public void testLineWithLabel() {
+		expectedPhoneNumber = "7177249371";
+		line = "Phone: (717) 724-9371";
+		lineContainsPhoneNumber(line, expectedPhoneNumber);
+	}
+	
+	@Test
+	public void testLineWithFaxNumber_returnsEmptyString() {
+		expectedPhoneNumber = "";
+		line = "Fax: (717) 633-8845";
+		lineContainsPhoneNumber(line, expectedPhoneNumber);
+	}
+	
+	@Test
+	public void testLineWithPhoneAndFaxNumber() {
+		expectedPhoneNumber = "7177249371";
+		line = String.format("Phone: (717) 724-9371 %nFax: (717) 633-8845");
+		lineContainsPhoneNumber(line, expectedPhoneNumber);
+	}
+	
+	@Test
+	public void testLineWithPhoneAndFaxNumberReversed() {
+		expectedPhoneNumber = "7177249371";
+		line = String.format("Fax: (717) 633-8845 %nTel: (717) 724-9371");
+		lineContainsPhoneNumber(line, expectedPhoneNumber);
+	}
+	
+	
+	@Test
+	public void testLineWithLabelSuffix() {
+		expectedPhoneNumber = "7177249371";
+		line = String.format("(717) 724-9371 Tel%n(717) 633-8845 Fax");
+		lineContainsPhoneNumber(line, expectedPhoneNumber);
+	}
+	
+	
+	
+	@Test
+	public void testLineWithCountryPrefix() {
+		expectedPhoneNumber = "17177249371";
+		line = "Phone: +1 (717) 724-9371";
+		lineContainsPhoneNumber(line, expectedPhoneNumber);
+	}
+
+	public void lineContainsPhoneNumber(String line, String expectedName) {
+		String actualName = phoneNumberParser.processLine(line);
+		assertEquals(expectedName, actualName);
+	}
 }
